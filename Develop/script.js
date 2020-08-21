ff2febf51ed3b4bc9fecf14b8de49ad7d6ebd1f7
nosideri <madi.stollmeyer@gmail.com>
2020-08-21 00:04:18 -0700

    finished password generator website

---
+++ Develop/script.js
@@ -0,0 +1,79 @@
+// Assignment code here
+function generatePassword() {
+  var password = "";
+  // Prompt for length of password, 8-128
+  var promptLength = window.prompt("How long would you like the password to be? [8-128]");
+  if (promptLength >= 8 && promptLength <= 128)
+  {
+    // Prompt for lowercase
+    var promptLowercase = window.confirm("Would you like lowercase letters?");
+
+    // Prompt for uppercase
+    var promptUppercase = window.confirm("Would you like uppercase letters?");
+
+    // Prompt for numeric
+    var promptNumeric = window.confirm("Would you like numbers?");
+
+    //Prompt for special characters
+    var promptSpecial = window.confirm("Would you like special characters?");
+
+    if (!promptLowercase && !promptUppercase && !promptNumeric && !promptSpecial) {
+      window.alert ("Please choose at least one criteria for your password!");
+      generatePassword();
+    }
+  }
+  else {
+    window.alert("Please choose a valid number between 8 and 128.");
+    generatePassword();
+  }
+  mainString = "";
+
+    upperString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    lowerString = "abcdefghijklmnopqrstuvwxyz";
+
+    numberString ="0123456789";
+
+    specialString = "!\"#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+
+    if(promptLowercase) {
+        mainString = mainString + lowerString;
+    }
+
+      if(promptUppercase) {
+          mainString = mainString + upperString;
+      }
+
+        if(promptNumeric) {
+          mainString = mainString + numberString;
+        }
+
+          if(promptSpecial) {
+            mainString = mainString + specialString;
+          }
+
+    for (i = 0; i < promptLength; i++) { 
+    var char = Math.floor(Math.random() * mainString.length + 1); 
+      password+= mainString.charAt(char);
+    }
+    return password;
+}
+
+
+
+// Alert with password
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
---
+++ README.md
@@ -0,0 +1,20 @@
+# Password Generator Starter Code
+
+## Purpose
+A website that generates passwords of lengths between 8-128 characters.
+Allows users the options to have lowercase, uppercase, numeric, and/or special characters.
+
+## Built With
+* HTML
+* CSS
+* Javascript
+
+## Website
+https://lernantino.github.io/run-buddy/
+Preview: http://prntscr.com/u3ana1
+
+## Contribution
+Made with ❤️ by Madison
+Starter code from https://github.com/coding-boot-camp/friendly-parakeet
+
+### ©️2020 nosideri
